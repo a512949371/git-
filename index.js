bef6490cba5122850ab1319f136b4a7e9868ab9f
a512949371 <512949371@qq.com>
2018-11-30 15:02:35 +0800

    rn学习demo

---
+++ index.js
@@ -0,0 +1,14 @@
+/** @format */
+import React,{Component} from 'react';
+import {AppRegistry} from 'react-native';
+//  import App from './App';
+import App from './App';
+import {name as appName} from './app.json';
+export default class Pagejump extends Component {
+    render() {
+        return (
+            <App/>
+        );
+    }
+}
+AppRegistry.registerComponent(appName, () => Pagejump);
